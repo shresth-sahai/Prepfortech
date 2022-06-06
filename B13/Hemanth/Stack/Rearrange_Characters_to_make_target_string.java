af6be9a183e99761ff72ee8f565677e1a97aac76
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2022-06-06 22:55:16 +0530

    Remove Characters to make target string

---
+++ B13/Hemanth/Stack/Rearrange_Characters_to_make_target_string.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/rearrange-characters-to-make-target-string/
+
+
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        int[] sc = new int[26];
+        int[] tc = new int[26];
+        for(int i=0;i<s.length();i++){
+            sc[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<target.length();i++){
+            tc[target.charAt(i)-'a']++;
+        }
+        int ans = s.length();
+        for(int i=0;i<26;i++){
+            if(tc[i]!=0) {
+                ans = Math.min(ans,sc[i]/tc[i]);
+            }
+        }
+        return ans;
+    }
+}
