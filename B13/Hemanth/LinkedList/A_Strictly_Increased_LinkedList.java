1941b5d3cbd03bab81247b669f76291b981dbf94
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2022-06-25 23:57:28 +0530

    A strictly Increased LinkedlIst

---
+++ B13/Hemanth/LinkedList/A_Strictly_Increased_LinkedList.java
@@ -0,0 +1,28 @@
+// https://binarysearch.com/problems/A-Strictly-Increasing-Linked-List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode forw = null;
+        
+        while(curr!=null){
+            forw = curr.next;
+            curr.next = prev;
+            
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+}
